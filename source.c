1e945106ea2fdf95d3c5ccd6417140f5aa5bff94
estes <hve0002@uah.edu>
2020-05-07 21:33:13 -0500

    Added comments to source.c

--- source.c
+++ source.c
@@ -5,12 +5,13 @@
 
 int main (int argc, char* argv[])
 {
-	if (argc != 3)
+	if (argc != 3) // Force the user to add mass and distance arguments when they launch the program
 	{
 		printf("You must enter the Mass(kg) and Fall Distance(m)\n");	
-		return 2;
+		return 2; // return with error
 	} 
 
+	// Initialize and calculate variables
 	double mass = atof(argv[1]);// kilograms
 	const double earthMass = 5.972 * pow(10, 24);
 	const double earthRadius = 6370000;
@@ -18,39 +19,46 @@ int main (int argc, char* argv[])
 	double initDistance = atof(argv[2]) + earthRadius;  // meters
 	double distance = initDistance;
 
-	double initVel = 0;
+	double initVel = 0;		// m/2
 	double vel = initVel;
 
-	double initTime = 0;
+	double initTime = 0;	// seconds
 	double time = initTime;
 
 	double gravity = 9.8; // m/s^2
 
 	double keneticE = 0;  // jouls
 
-	double acceleration = 0;
+	double acceleration = 0;  // m/s^2
 
-	double gravityDifference = 0;
+	double gravityDifference = 0; 
 
 	double impactForce = 0; // Newtons
 
-	printf("%.2f\n", gravity);
-
-	// Drop
+	// Drop point
 	acceleration = gravity;
-	while(distance > earthRadius)
+	while(distance > earthRadius)  // while loop collects stats for the object falling every second 
 	{
-		gravityDifference = 1 / (distance / 2);
+		// Calculate the change in gravity based on distance from the earths core and apply it to acceleration
+		gravityDifference = 1 / (distance / 2);   
 		gravity = gravity - (gravityDifference * gravity);  
 		acceleration = gravity;
-		vel = initVel + (acceleration * time);	
-		keneticE = (mass * (pow(vel , 2))) * 0.5;		
-		distance = distance - vel;
-		impactForce = keneticE / (distance - earthRadius);
+
+		vel = initVel + (acceleration * time);	// Calculate the velocity
+
+		keneticE = (mass * (pow(vel , 2))) * 0.5;	// Find the amount of current kenetic enegry the object has this second
+
+		distance = distance - vel;  // Distance change to show the object falling
+
+		impactForce = keneticE / (distance - earthRadius);  // Calculate the force of impact the object has on the earths surface
+
+		//Display Information to Terminal
 		printf("mass: %.2fkg      distance: %.2fm       time: %.2fs      velocity: %.2fm/s      kenetic energy: %.2fj       acceleration: %.8fm/s\n", mass, distance - earthRadius, time, vel, keneticE, acceleration);
 
-		time++;
+		time++;  // increase time by one second
 	}
+
+		// Print out final results and stats
 		printf("The object hit the ground with %.2f Newtons of impact force.\n",impactForce);
 
 	return 0;
