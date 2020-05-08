963973a1a65acf4a99356dea87e0bb5d36c72d50
estes <hve0002@uah.edu>
2020-05-07 21:20:11 -0500

    Added Readme and Added more statistics to be displayed

--- source.c
+++ source.c
@@ -1,50 +1,55 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
-int main (int argc, int argv[])
+int main (int argc, char* argv[])
 {
-	double mass = 10;		// mass is kg
+	if (argc != 3)
+	{
+		printf("You must enter the Mass(kg) and Fall Distance(m)\n");	
+		return 2;
+	} 
 
+	double mass = atof(argv[1]);// kilograms
 	const double earthMass = 5.972 * pow(10, 24);
 	const double earthRadius = 6370000;
 
-	double initDistance = 100 + earthRadius;  // distance is m
+	double initDistance = atof(argv[2]) + earthRadius;  // meters
 	double distance = initDistance;
 
-	double initVel = 10;
+	double initVel = 0;
 	double vel = initVel;
 
 	double initTime = 0;
 	double time = initTime;
 
-	double gravity = (mass * earthMass)/(pow(initDistance, 2));
+	double gravity = 9.8; // m/s^2
 
-	double keneticE = 0;
+	double keneticE = 0;  // jouls
 
 	double acceleration = 0;
 
-	double stalledDistance;
-	
-	double impactForce;
+	double gravityDifference = 0;
 
-	printf("mass: %.2f      distance: %.2f       time: %.2f      velocity: %.2f      kenetic energy: %.2f       acceleration: %.2f \n", mass, distance - earthRadius, time, vel, keneticE, acceleration);
+	double impactForce = 0; // Newtons
+
+	printf("%.2f\n", gravity);
 
 	// Drop
-	acceleration = 9.8;
+	acceleration = gravity;
 	while(distance > earthRadius)
 	{
+		gravityDifference = 1 / (distance / 2);
+		gravity = gravity - (gravityDifference * gravity);  
+		acceleration = gravity;
 		vel = initVel + (acceleration * time);	
-		keneticE = (mass * (pow(vel , 2))) * 0.5;		//kenertic Energy in J
+		keneticE = (mass * (pow(vel , 2))) * 0.5;		
 		distance = distance - vel;
-		if (distance <= earthRadius) //Impact
-		{
-			impactForce = keneticE / (earthRadius - distance);
-			stalledDistance = earthRadius - distance;
-		}
+		impactForce = keneticE / (distance - earthRadius);
+		printf("mass: %.2fkg      distance: %.2fm       time: %.2fs      velocity: %.2fm/s      kenetic energy: %.2fj       acceleration: %.8fm/s\n", mass, distance - earthRadius, time, vel, keneticE, acceleration);
 
 		time++;
-		printf("mass: %.2f      distance: %.2f       time: %.2f      velocity: %.2f      kenetic energy: %.2f       acceleration: %.2f \n", mass, distance - earthRadius, time, vel, keneticE, acceleration);
 	}
 		printf("The object hit the ground with %.2f Newtons of impact force.\n",impactForce);
 
