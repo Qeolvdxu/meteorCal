67cb33eb189d09deb9285ee083f19a75594c5292
estes <hve0002@uah.edu>
2020-05-14 09:18:47 -0500

    added temp system commands to sync simulated time with real time

--- source.c
+++ source.c
@@ -1,15 +1,26 @@
 #include <math.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+void clear_screen()
+{
+#ifdef WINDOWS
+    system("cls");
+#else
+    // Assume POSIX
+    system ("clear");
+#endif
+}
 
 int main (int argc, char* argv[])
 {
 	if (argc != 3) // Force the user to add mass and distance arguments when they launch the program
 	{
-		printf("You must enter the Mass(kg) and Fall Distance(m)\n");	
+		printf("You must enter the Mass(kg) and Fall Distance(m)\n Ex: meteorSim 10 100\n");	
 		return 2; // return with error
 	} 
+	system("clear");
 
 	// Initialize and calculate variables
 	double mass = atof(argv[1]);// kilograms
@@ -23,7 +34,7 @@ int main (int argc, char* argv[])
 	double vel = initVel;
 
 	double initTime = 0;	// seconds
-	double time = initTime;
+	double simTime = initTime;
 
 	double gravity = 9.8; // m/s^2
 
@@ -31,7 +42,7 @@ int main (int argc, char* argv[])
 
 	double acceleration = 0;  // m/s^2
 
-	double gravityDifference = 0; 
+	double gravityDifference = 0; //amount that will be subtracted from gravity
 
 	double impactForce = 0; // Newtons
 
@@ -39,23 +50,25 @@ int main (int argc, char* argv[])
 	acceleration = gravity;
 	while(distance > earthRadius)  // while loop collects stats for the object falling every second 
 	{
-		// Calculate the change in gravity based on distance from the earths core and apply it to acceleration
-		gravityDifference = 1 / (distance / 2);   
-		gravity = gravity - (gravityDifference * gravity);  
-		acceleration = gravity;
+			// Calculate the change in gravity based on distance from the earths core and apply it to acceleration
+			gravityDifference = 1 / (distance / 2);   
+			gravity = gravity - (gravityDifference * gravity);  
+			acceleration = gravity;
 
-		vel = initVel + (acceleration * time);	// Calculate the velocity
+			vel = initVel + (acceleration * simTime);	// Calculate the velocity
 
-		keneticE = (mass * (pow(vel , 2))) * 0.5;	// Find the amount of current kenetic enegry the object has this second
+			keneticE = (mass * (pow(vel , 2))) * 0.5;	// Find the amount of current kenetic enegry the object has this second
 
-		distance = distance - vel;  // Distance change to show the object falling
+			distance = initDistance - (vel * simTime);  // Distance change to show the object falling
 
-		impactForce = keneticE / (distance - earthRadius);  // Calculate the force of impact the object has on the earths surface
+			impactForce = keneticE / (distance - earthRadius);  // Calculate the force of impact the object has on the earths surface
 
-		//Display Information to Terminal
-		printf("mass: %.2fkg      distance: %.2fm       time: %.2fs      velocity: %.2fm/s      kenetic energy: %.2fj       acceleration: %.8fm/s\n", mass, distance - earthRadius, time, vel, keneticE, acceleration);
+			//Display Information to Terminal
+			printf("mass: %.2fkg      distance: %.2fm       time: %.2fs      velocity: %.2fm/s      kenetic energy: %.2fj       acceleration: %.8fm/s\n", mass, distance - earthRadius, time, vel, keneticE, acceleration);
 
-		time++;  // increase time by one second
+			simTime++;  // increase simTime by one second
+			system("sleep 1");
+			system("clear");
 	}
 
 		// Print out final results and stats
